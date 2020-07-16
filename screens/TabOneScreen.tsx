a3a29be1246e0afc34388df2d8d75ae0c7fb6292
Samuel Favarin <samuel.favarin@conectanuvem.com.br>
2020-07-16 08:01:06 -0300

    listagem

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,30 +1,74 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  SectionList
+} from "react-native";
+import Constants from "expo-constants";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Empty Page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+      />
+    </SafeAreaView>
   );
 }
+
+const DATA = [
+  {
+    data: ["Clean Code", "Data Science from Zero", "How do friends and influence people", "Arquitetura limpa", "Padrões de Projetos em Python", "Php o guia definitivo"]
+  }
+];
+
+const Item = ({ title }) => (
+
+  <TouchableOpacity >
+    <View style={styles.item}>
+      <Ionicons size={20} style={styles.icon} name="ios-bookmark" />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+
+  </TouchableOpacity>
+);
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16
   },
+  item: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginVertical: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    flexDirection: "row"
+  },
+
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 16,
+    color: "#3b3a39"
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  icon: {
+    marginHorizontal: 8,
+    color: 'red'
+  }
+
 });
